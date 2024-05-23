13f3cf1850b5d3706226f26893c5e2f61068dd9f
SupperGabor <gabor1415@gmail.com>
2024-05-23 15:04:25 +0200

    how many button now get cleared after search

--- scripts.js
+++ scripts.js
@@ -72,6 +72,7 @@ $(document).ready(function () {
   recipesButton.on("click", function () {
       const howMany = howManyInput.val();
       displayRandomMeals(howMany);
+      $(howManyInput).val("")
   });
 
   function getRecipe() {
